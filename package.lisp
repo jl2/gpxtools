077b95bc22e46c37ff390ec282789f0a3e37331f
Jeremiah LaRocco <jeremiah.larocco@gmail.com>
2016-07-29 20:54:08 -0600

    Add simplification.

--- package.lisp
+++ package.lisp
@@ -10,8 +10,23 @@
            #:elevation-diff
            #:meters-to-feet
            #:meters-to-miles
+           #:get-summary
            #:summarize
            #:collect-points
            #:elevation-plot
+           #:find-loop
+           #:simplify
+           #:write-gpx
+           #:gpx-file-tracks
+           #:gpx-track-segments
+           #:gpx-segment-points
+           #:gpx-segment-point-count
+
+           #:gpx-point-lat
+           #:gpx-point-lon
+           #:gpx-point-ele
+           #:gpx-point-time
+
+           #:gpx-file-from-track
            ))
 
